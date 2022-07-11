3aff54dc4a9776cca8641da8c132295ad5b914fa
unano <919659233@qq.com>
2022-07-11 17:23:12 +0800

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -19,17 +19,17 @@ app.use("/api/user", userRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/chat", chatRoutes);
 
-// const __dirname1 = path.resolve();
-//   if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname1, "/client/build")));
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-//     });
-//   } else {
-//     app.get("/", (req, res) => {
-//       res.send("API is running..");
-//     });
-//   }
+const __dirname1 = path.resolve();
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+    });
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
+  }
 
 try {
   mongoose
